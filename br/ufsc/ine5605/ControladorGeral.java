806b56d48ff006198ead6a5ec3b5ea0f9f218ac3
GuilhermeBoczkovski <40305138+GuilhermeBoczkovski@users.noreply.github.com>
2018-11-19 07:34:20 -0200

    Add files via upload

---
+++ br/ufsc/ine5605/ControladorGeral.java
@@ -0,0 +1,59 @@
+package br.ufsc.ine5605.controladores;
+;
+
+import br.ufsc.ine5605.telas.TelaMenuInicial;
+import br.ufsc.ine5605.telas.TelaInicio;
+import br.ufsc.ine5605.telas.TelaSavedGame;
+
+public class ControladorGeral {
+    
+    private TelaInicio telaInicio;
+    private TelaMenuInicial telaMenuInicial;
+    private TelaSavedGame telaSavedGame;
+    private ControladorPrincipal controladorPrincipal;
+    private static ControladorGeral instancia;
+    
+    private ControladorGeral(){
+        this.telaMenuInicial = new TelaMenuInicial();
+        this.menuInicial();
+    }
+    
+    public static ControladorGeral getInstance(){
+        if(instancia == null){
+            instancia = new ControladorGeral();
+        }
+        return instancia;
+    }
+    
+    public void continuarComJogador(int indiceJogador){
+        ControladorPrincipal.getInstance().continuarComJogador(indiceJogador);
+    }
+    
+    public void excluirJogador(int indiceJogador){
+        ControladorPrincipal.getInstance().excluiJogador(indiceJogador);
+    }
+    
+    public void continuar(){
+        this.telaSavedGame = new TelaSavedGame(ControladorPrincipal.getInstance().getListaJogadoresSalvos());
+        this.telaSavedGame.mostraTela();
+    }
+    
+    public void menuInicial(){
+        this.telaMenuInicial.mostraTelaMenuInicial();
+    }
+    
+    public void inicio(){
+        try{
+            this.telaInicio = new TelaInicio();
+            telaInicio.mostraTelaInicio();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+    public void play(String nome){
+        ControladorPrincipal.getInstance(nome, true);
+        ControladorPrincipal.getInstance().escolheEncontro();
+    }
+    
+}
